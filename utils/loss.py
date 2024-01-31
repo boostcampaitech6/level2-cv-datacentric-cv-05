88f0c2297cb92a5c32a259326879cd445076507a
jinida <qkqh9912@gmail.com>
2024-01-31 09:39:42 +0900

    feat: add FP16 functionality

--- utils/loss.py
+++ utils/loss.py
@@ -8,14 +8,14 @@ def get_dice_loss(gt_score, pred_score):
 
 def get_geo_loss(gt_geo, pred_geo):
     d1_gt, d2_gt, d3_gt, d4_gt, angle_gt = torch.split(gt_geo, 1, 1)
-    d1_pred, d2_pred, d3_pred, d4_pred, angle_pred = torch.split(pred_geo, 1, 1)
+    d1_pred, d2_pred, d3_pred, d4_pred, angle_pred = torch.split(pred_geo.type(torch.float32), 1, 1)
     area_gt = (d1_gt + d2_gt) * (d3_gt + d4_gt)
     area_pred = (d1_pred + d2_pred) * (d3_pred + d4_pred)
     w_union = torch.min(d3_gt, d3_pred) + torch.min(d4_gt, d4_pred)
     h_union = torch.min(d1_gt, d1_pred) + torch.min(d2_gt, d2_pred)
     area_intersect = w_union * h_union
     area_union = area_gt + area_pred - area_intersect
-    iou_loss_map = -torch.log((area_intersect + 1.0) / (area_union + 1.0) + 6e-8)
+    iou_loss_map = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
     angle_loss_map = 1 - torch.cos(angle_pred - angle_gt)
     return iou_loss_map, angle_loss_map
 
@@ -33,7 +33,7 @@ class EASTLoss(nn.Module):
 
         angle_loss = torch.sum(angle_loss_map * gt_score) / torch.sum(gt_score)
         angle_loss *= self.weight_angle
-        iou_loss = torch.sum(iou_loss_map * gt_score) / torch.sum(gt_score)
+        iou_loss = torch.sum(iou_loss_map * gt_score) / (torch.sum(gt_score) + 1e-6)
         geo_loss = angle_loss + iou_loss
         total_loss = classify_loss + geo_loss
 
